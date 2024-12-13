628c19052add3f1b6c7884bf27de9ea9e4a3ab30
guydavid105 <87449924+guydavid105@users.noreply.github.com>
2024-12-13 05:19:34 +0000

    Update access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -134,6 +134,27 @@ def plot_price_area_corr(merged_df):
   plt.ylabel('Price')
   plt.show()
 
+def download_census_data(year, code, base_dir=''):
+  year = str(year)
+  url = f'https://www.nomisweb.co.uk/output/census/{year}/census{year}-{code.lower()}.zip'
+  extract_dir = os.path.join(base_dir, os.path.splitext(os.path.basename(url))[0])
+
+  if os.path.exists(extract_dir) and os.listdir(extract_dir):
+    print(f"Files already exist at: {extract_dir}.")
+    return
+
+  os.makedirs(extract_dir, exist_ok=True)
+  response = requests.get(url)
+  response.raise_for_status()
+
+  with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
+    zip_ref.extractall(extract_dir)
+
+  print(f"Files extracted to: {extract_dir}")
+
+def load_census_data(year, code, level='msoa'):
+  return pd.read_csv(f'census{year}-{code.lower()}/census{year}-{code.lower()}-{level}.csv')
+
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
     raise NotImplementedError
